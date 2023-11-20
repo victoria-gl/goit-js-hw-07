31cda8a86af05d9946b984c84a216f34a7890af9
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-20 22:55:24 +0100

    start-js-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const container = document.querySelector(".gallery")
+
+// console.log(container);
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+container.addEventListener("click", handleClick);
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+        <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      data-id="${description}"
+    />
+  </a>
+</li>
+        `).join("")
+};
+
+// console.log(createMarkup(galleryItems));
+
+function handleClick(event) {
+            if (event.target === event.currentTarget) {
+                return;
+            }
+    
+    event.preventDefault()
+
+    const galleryItem = event.target.closest(".gallery__image");
+            console.log("currentTarget", event.currentTarget);
+            console.log("target", event.target);
+            // console.log(galleryItem);
+    
+    const itemDescr = galleryItem.dataset.id
+    console.log(itemDescr);
+
+    const originalSRC = event.target.dataset.source
+    console.log(originalSRC);
+
+    //Модальне вікно
+
+    const instance = basicLightbox.create(`
+      <img src="${originalSRC}"/>
+
+`)
+     instance.show()
+}
+
